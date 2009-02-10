af152b1e16650003957b0987cba8ac14a43619ae
Nick Merwin <mer@mermac-2.local>
2009-02-09 20:24:58 -0800

    save after updating auth stuff

--- lib/camatar.rb
+++ lib/camatar.rb
@@ -60,6 +60,7 @@ module Camatar
           @camatar_video = Camatar::Api::Video.create :max_duration => Camatar::default_duration
           self.send "#{camatar_opts[:token_column]}=", @camatar_video.token
           self.send "#{camatar_opts[:max_duration_column]}=", @camatar_video.max_duration
+          save
         rescue ActiveResource::ServerError
           
         rescue ActiveResource::UnauthorizedAccess, ActiveResource::ForbiddenAccess
