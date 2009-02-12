03330cc483cb23c483e4a712c3630829500d8baf
Nick Merwin <mer@mermac-2.local>
2009-02-11 18:59:41 -0800

    updated install.rb

--- install.rb
+++ install.rb
@@ -2,5 +2,5 @@
 require 'fileutils'
 
 FileUtils.cp File.dirname(__FILE__) + '/camatar.swf', "/../../../public"
-FileUtils.cp File.dirname(__FILE__) + '/camatar_example.yml', "/../../../config/camatar.yml"
+FileUtils.cp File.dirname(__FILE__) + '/camatar_example.yml', "/../../../config"
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
