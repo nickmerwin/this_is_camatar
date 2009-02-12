e79e395ca3f550f8a211807e18e3ccd191a78dc8
Nick Merwin <mer@mermac-2.local>
2009-02-11 19:06:28 -0800

    updated camatar_example.yml

--- install.rb
+++ install.rb
@@ -2,5 +2,5 @@
 require 'fileutils'
 pwd = File.dirname(__FILE__)
 FileUtils.cp  "#{pwd}/camatar.swf", "#{pwd}/../../../public"
-FileUtils.cp "#{pwd}/camatar_example.yml", "#{pwd}/../../../config"
+FileUtils.cp "#{pwd}/camatar_example.yml", "#{pwd}/../../../config/camatar.yml"
 puts IO.read(File.join(pwd, 'README.markdown'))
